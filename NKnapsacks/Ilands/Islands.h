836177675ad60ea56e8419a1a5015a41029207e0
Raizner <raizner@gmail.com>
2015-05-03 18:10:52 +0300

    Init

---
+++ NKnapsacks/Ilands/Islands.h
@@ -0,0 +1,33 @@
+#ifndef _ISLAND_H
+//#include "MiscHeader.h"
+//#include "Strings.cpp"
+//#include "Queens.cpp"
+#include "knapsack.cpp"
+
+
+template <class T>
+class Islands{
+	
+#pragma region members
+
+	T* islands;
+	int numberOfIslands;
+	int populationPerIsland;
+	double elitismeRate;
+	int epocLength;
+	int migrationFromEachIsland;
+
+#pragma endregion
+	
+
+
+public:
+
+	Islands(int numberOfIslands, int populationPerIsland, double elitismeRate, int epocLength, int migrationFromEachIsland, ...) ;
+	void evolve();
+	void MigrationBetweenIslands();
+
+
+
+};
+#endif // !_ISLAND_H
